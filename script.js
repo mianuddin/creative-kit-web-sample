fe6b290785e21fe1b8c6ae47581ed458f50419da
Mian Uddin <mianuddin@gmail.com>
2019-10-10 13:30:22 -0700

    feat: update titles

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return word.replace(word[0], word[0].toUpperCase());
+  }).join(' ');
+}
+
 new Vue({
   el: '#app',
   data: {
@@ -9,13 +15,12 @@ new Vue({
     const urlParams = new URLSearchParams(window.location.search);
     this.selected = urlParams.get('item');
 
-    document.title = `${this.firstName}'s Scrapbook`;
-
     fetch('./contents.yaml')
       .then(response => response.text())
       .then(data => this.contents = jsyaml.safeLoad(data));
   },
   updated: function () {
+    document.title = `${this.contents[this.selected] ? `${titleCase(this.selected)} | ` : ''} ${this.firstName}'s Scrapbook`;
     VanillaTilt.init(document.querySelectorAll("[data-tilt]"));
   }
 })
